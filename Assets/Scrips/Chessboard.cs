64535c89f5da1a72e4cbbbf4b774dc0ec99d5ffa
Victor González <vgonzalezvictor0@gmail.com>
2022-10-07 20:23:14 -0500

    Integration with 3d board and select tiles

--- Assets/Scrips/Chessboard.cs
+++ Assets/Scrips/Chessboard.cs
@@ -6,15 +6,20 @@ public class Chessboard : MonoBehaviour
 {
     [Header("Art stuff")]
     [SerializeField] private Material tileMaterial;
+    [SerializeField] private float tileSize = 1.0f;
+    [SerializeField] private float yOffset = 0.2f;
+    [SerializeField] private Vector3  boardCenter = Vector3.zero;
+
     //LOGIC
     private const int TILE_COUNT_X = 8;
     private const int TILE_COUNT_Y = 8;
     private GameObject[,] tiles;
     private Camera currentCamera;
     private Vector2Int currentHover;
+    private Vector3 bounds;
 
     private void Awake(){
-        GenerateAllTiles(1, TILE_COUNT_X, TILE_COUNT_Y);
+        GenerateAllTiles(tileSize, TILE_COUNT_X, TILE_COUNT_Y);
     }
 
     private void Update() 
@@ -55,14 +60,19 @@ public class Chessboard : MonoBehaviour
             }
         }
     }
+    
     // Generate Board
     private void GenerateAllTiles(float tileSize, int tileCountX, int tileCountY)
     {
+        yOffset += transform.position.y;
+        bounds = new Vector3((tileCountX / 2 ) * tileSize, 0 , (tileCountX / 2) * tileSize) + boardCenter;
+
         tiles = new GameObject[tileCountX, tileCountY];
         for (int x = 0; x < tileCountX; x++)
             for (int y = 0; y < tileCountY; y++)
                 tiles[x, y] = GenerateSingleTile(tileSize, x, y);
     }
+
     private GameObject GenerateSingleTile(float tileSize, int x, int y)
     {
        GameObject tileObject = new GameObject(string.Format("X:{0}, Y:{1}", x, y));
@@ -73,13 +83,13 @@ public class Chessboard : MonoBehaviour
         tileObject.AddComponent<MeshRenderer>().material = tileMaterial;
 
         Vector3[] vertices = new Vector3[4];
-        vertices[0] = new Vector3(x * tileSize, 0, y * tileSize);
-        vertices[1] = new Vector3(x * tileSize, 0, (y+1) * tileSize);
-        vertices[2] = new Vector3((x+1) * tileSize, 0, y * tileSize);
-        vertices[3] = new Vector3((x+1) * tileSize, 0, (y+1) * tileSize);
+        vertices[0] = new Vector3(x * tileSize, yOffset, y * tileSize) - bounds;
+        vertices[1] = new Vector3(x * tileSize, yOffset, (y+1) * tileSize) - bounds;
+        vertices[2] = new Vector3((x+1) * tileSize, yOffset, y * tileSize) - bounds;
+        vertices[3] = new Vector3((x+1) * tileSize, yOffset, (y+1) * tileSize) - bounds;
 
         int[] tris = new int[] { 0, 1, 2, 1, 3, 2};
-
+        
         mesh.vertices = vertices;
         mesh.triangles = tris;
         mesh.RecalculateNormals();
