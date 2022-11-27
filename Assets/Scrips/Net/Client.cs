9e6efdb13984587b575406b53d9029403b85d4b2
Victor Gonzalez <vgonzalezvictor0@gmail.com>
2022-11-27 15:40:05 -0600

    Client script

--- Assets/Scrips/Net/Client.cs
+++ Assets/Scrips/Net/Client.cs
@@ -1,18 +1,123 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Unity.Networking.Transport;
 using UnityEngine;
 
 public class Client : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Singleton implemetation
+    public static Client Instance { set; get; }
+    private void Awake()
     {
-        
+        Instance = this;
+
+    }
+    #endregion
+
+
+    public NetworkDriver driver;
+    private NetworkConnection connection;
+
+    private bool isActive = false;
+
+    public Action connectionDropped;
+
+    //Methods
+    public void Init(string ip, ushort port)
+    {
+        driver = NetworkDriver.Create();
+        NetworkEndPoint endpoint = NetworkEndPoint.Parse(ip, port);
+
+        connection = driver.Connect(endpoint);
+
+        Debug.Log("Attemping to connect to Server On " + endpoint.Address);
+
+        isActive = true;
+
+        RegisterToEvent();
+    }
+    public void Shutdown()
+    {
+        if (isActive)
+        {
+            UnRegisterToEvent();
+            driver.Dispose();
+            isActive = false;
+            connection = default(NetworkConnection);
+        }
+    }
+    public void OnDestroy()
+    {
+        Shutdown();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if (!isActive)
+            return;
+
+        driver.ScheduleUpdate().Complete();
+        CheckAlive();
+
+        UpdateMessagePump();
     }
+    private void CheckAlive()
+    {
+        if (!connection.IsCreated && isActive)
+        {
+            Debug.Log("Something went wrong, lost connection to server");
+            connectionDropped?.Invoke();
+            Shutdown();
+        }
+    }
+
+    private void UpdateMessagePump()
+    {
+        DataStreamReader stream;
+        NetworkEvent.Type cmd;
+        while ((cmd = connection.PopEvent(driver, out stream)) != NetworkEvent.Type.Empty)
+        {
+            if (cmd == NetworkEvent.Type.Data)
+            {
+                if (cmd == NetworkEvent.Type.Connect)
+                {
+                    //SendToServer(new NetWelcome()) ;
+                }
+            }
+            else if (cmd == NetworkEvent.Type.Data)
+            {
+                //NetUtility.OnData(stream, default(NetworkConnection));
+            }
+            else if (cmd == NetworkEvent.Type.Disconnect)
+            {
+                Debug.Log("client got disconnected from server");
+                connection = default(NetworkConnection);
+                connectionDropped?.Invoke();
+                Shutdown();
+            }
+        }
+    }
+
+    public void SendToServer(NetMessage msg)
+    {
+        DataStreamWriter writer;
+        driver.BeginSend(connection, out writer);
+        // msg.Serialize(ref writer);
+        driver.EndSend(writer);
+    }
+
+    //event parsing
+
+    private void RegisterToEvent()
+    {
+        //NetUtility.C_KEEP_ALIVE += OnkeppAlive;
+    }
+    private void UnRegisterToEvent()
+    {
+        //NetUtility.C_KEEP_ALIVE -= OnkeppAlive;
+    }
+    private void OnkeepAlive(NetMessage nm)
+    {
+        SendToServer(nm);
+    }
+
 }
