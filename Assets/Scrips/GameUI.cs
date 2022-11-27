3fb6e7c97a01b33259f8c03ee655db787c39ae30
Victor Gonzalez <vgonzalezvictor0@gmail.com>
2022-11-27 12:46:09 -0600

    Navegation UI LocalGame incomplete

--- Assets/Scrips/GameUI.cs
+++ Assets/Scrips/GameUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,7 @@ public class GameUI : MonoBehaviour
 
     [SerializeField] private Animator menuAnimator;
 
+    public Action<bool> SetlocalGame;
     private void Awake()
     {
         Instance = this;
@@ -17,6 +19,7 @@ public class GameUI : MonoBehaviour
     public void OnLocalGameButton()
     {
         menuAnimator.SetTrigger("InGameMenu");
+        SetlocalGame?.Invoke(true);
     }
 
     public void OnOnlineGameButton()
@@ -26,11 +29,13 @@ public class GameUI : MonoBehaviour
 
     public void OnOnlineHostButton()
     {
+        SetlocalGame?.Invoke(false);
         menuAnimator.SetTrigger("HostMenu");
     }
 
     public void OnOnlineConnectButton()
     {
+        SetlocalGame?.Invoke(false);
         Debug.Log("OnOnlineConnectButton"); // $$
     }
 
